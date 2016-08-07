8109f9b6a19b36458b4ee9a017b7b854acee976f
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2016-08-07 13:51:56 +0900

    Implement s3 client example

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( printBuckets
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Lens
+import           Network.AWS
+import           Network.AWS.S3
+import           System.Environment (getArgs)
+import           System.IO
+
+example :: FilePath -> IO ListBucketsResponse
+example f = do
+    e <- newEnv Tokyo $ FromFile "default" f
+    l <- newLogger Debug stdout
+    runResourceT . runAWS (e & envLogger .~ l) $ send listBuckets
+
+printBuckets :: IO ()
+printBuckets = do
+    args <- getArgs
+    let f = args !! 0
+    ex <- example f
+    print $ [ x ^. bName | x <- ex ^. lbrsBuckets ]
