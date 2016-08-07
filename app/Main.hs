4de611f3dd2c71d05ceb0b2b590162b9fada568b
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2016-08-07 16:06:55 +0900

    Credential from environment variable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Lib
+import           Control.Lens
+import           Network.AWS
+import           Network.AWS.S3
+import           System.IO
+
+example :: IO ListBucketsResponse
+example = do
+    e <- newEnv Tokyo $ FromEnv "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY" Nothing
+    runResourceT . runAWS e $ send listBuckets
+
+printBuckets :: IO ()
+printBuckets = do
+    ex <- example
+    print $ [ x ^. bName | x <- ex ^. lbrsBuckets ]
 
 main :: IO ()
 main = printBuckets
